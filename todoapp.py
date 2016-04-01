f601c62f296754f26f98149fa84a754e21451cc3
meshack nyabiba <meshack nyabiba>
2016-04-01 11:02:38 +0300

    Created a todo model

--- todoapp.py
+++ todoapp.py
@@ -1,6 +1,25 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+app = Flask(__name__)
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+	"""docstring for Todo"""
+	__tablename__ = 'todos'
+	id = db.Column('todo_id', db.Integer, primary_key=True)
+	title = db.Column(db.String(60))
+	text = db.Column(db.String)
+	done = db.Column(db.Boolean)
+	pub_date = db.Column(db.Datetime)
+
+	def __init__(self, title, text):
+		self.title = title
+		self.text = text
+		self.done = False
+		self.pub_date = datetime.utcnow()
+		
 
 @app.route('/')
 def index():
@@ -8,3 +27,4 @@ def index():
 
 if __name__ == '__main__':
 	app.run()
+
